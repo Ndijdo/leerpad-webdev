245bfc37b9aea072c7d47f4e2bab6e0a255bb552
N'djido <ardozuidberg@gmail.com>
2026-04-20 13:09:14 +0200

    Lesson 3 Completed JS

--- fase2/js/main.js
+++ fase2/js/main.js
@@ -1,4 +1,23 @@
-console.log("Hello, World!");
-console.log(typeof "ardo");
-console.log(typeof 42);
-console.log(typeof true);
+// strings
+const myVariable = "Mathematics";
+
+
+// The lengt property 
+console.log(myVariable.length);
+
+// String methods
+console.log(myVariable.charAt(6));
+console.log(myVariable.indexOf("Mat"));
+console.log(myVariable.lastIndexOf("at"));
+console.log(myVariable.slice(5,8));
+console.log(myVariable.slice(5));
+console.log(myVariable.slice(5,2));
+console.log(myVariable.slice(5,6));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("div"));
+console.log(myVariable.includes("mat"));
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""));
+console.log("John,Joe,Dave".split(","));
+console.log("Every good boy does fine".split(" "));
