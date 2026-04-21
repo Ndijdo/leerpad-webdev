b52b2c104dfb21bf996422ddbe0483cfbc093572
N'djido <ardozuidberg@gmail.com>
2026-04-21 12:54:08 +0200

    Lesson 4 Completed JS

--- fase2/js/main.js
+++ fase2/js/main.js
@@ -1,23 +1,59 @@
-// strings
-const myVariable = "Mathematics";
+// Numbers
+
+// An interger is a whole number
+const myNumber = 42;
+
+// A number with a decismal point is a float which references the "floating point"
+const myFloat = 42.0123;
+
+const myString = "42.123abc";
+
+console.log(myNumber);
+console.log(myFloat);
+console.log(myNumber === myFloat);
+console.log(myNumber === myString);
+console.log(myString + 3);
+console.log(Number(myString) + 3);
+console.log(Number(myString) === myNumber);
+console.log(Number("Ardo"));
+console.log(Number(undefined));
+console.log(Number(true));
+console.log(Number(false));
+
+// Number Methods
+// The Number.isinterger() method determines whether the passed value is an integer.
+console.log(Number.isInteger(myString));
 
 
-// The lengt property 
-console.log(myVariable.length);
+// The Number.parseFloat() method parses an argumnet and returns a floating point number. 
+// If a number cannot be parsed from the argument, it returns NaN.
+console.log(Number.parseFloat(myNumber));
 
-// String methods
-console.log(myVariable.charAt(6));
-console.log(myVariable.indexOf("Mat"));
-console.log(myVariable.lastIndexOf("at"));
-console.log(myVariable.slice(5,8));
-console.log(myVariable.slice(5));
-console.log(myVariable.slice(5,2));
-console.log(myVariable.slice(5,6));
-console.log(myVariable.toUpperCase());
-console.log(myVariable.toLowerCase());
-console.log(myVariable.includes("div"));
-console.log(myVariable.includes("mat"));
-console.log(myVariable.split("e"));
-console.log(myVariable.split(""));
-console.log("John,Joe,Dave".split(","));
-console.log("Every good boy does fine".split(" "));
+
+// The toFixed() method formats a number according to how many decimal points you provide as the parameter.
+console.log(myFloat.toFixed(2));
+
+
+// The Number.parseInt() method parses a string argument and returns an integer.
+console.log(Number.parseInt(myString));
+
+
+// The toString() method returns a string representing the number.
+console.log(myFloat.toString());
+console.log(typeof myFloat.toString());
+
+
+// Chaining = Using several methods chained together.
+console.log(Number.parseFloat("42.123abc").toFixed(2).toString());
+
+ 
+//NaN is an acronym for Not a Number. 
+console.log(Number("Ardo")); 
+
+
+// The Number.isNaN() method determines whether the passed value is NaN and its type is Number.
+console.log(Number.isNaN("Ardo"))
+
+
+// The global isNaN() function determines whether a value is NaN or not.
+console.log(isNaN("Ardo"));
