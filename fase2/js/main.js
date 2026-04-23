17e3a89d89961e2f8aea4df0d900df6c0dc552a9
N'djido <ardozuidberg@gmail.com>
2026-04-23 14:10:11 +0200

    Lesson 10 Completed JS

--- fase2/js/main.js
+++ fase2/js/main.js
@@ -1,40 +1,16 @@
-// conditionals: Ternary Operator
+// User input
 
-//syntax
-// condition? ifTrue : iffalse;
+alert("Hello World!")
 
-let soup = "chicken noodle soup";
-let response = soup ? "Yes, we have soup." : "sorry, no soup todya.";
-console.log(response);
+ let myBoolean =confirm(" OK === True\nCancel === False")
+ console.log(myBoolean);
 
+ let name = prompt("Plesae enter your name");
+ if (name) {
+    console.log(name.length);
+    console.log(name.trim().length);
+    console.log(name.trim());
+ } else { 
+    console.log("You didn't enter your names");
+ }
 
-// ternary operator with multiple conditions
-let soup = "chicken noodle soup";
-let isCustomerBanned = true; 
-let soupAccess = isCustomerBanned 
-? "Sorry, no soup for you!" 
-: soup 
-? `Yes, we have ${soup} today.`
-: "Sorry, no soup today.";
-console.log(soupAccess);
-
-
-// grade calculator using ternary operator
-let testScore = 90; 
-let myGrade = testScore > 89 ? "A"
-: testScore > 79 ? "B"
-: testScore > 69 ? "C"
-: testScore > 59 ? "D"
-: "F";
-console.log(`My test grade is a ${myGrade}.`);
-
-
-// rock, paper, scissors game using ternary operator
-let playerOne = "rock";
-let computer = "rock"; 
-let result = playerOne === computer ? "Tie game!"
-: playerOne === "rock" && computer === "scissors" ? "Player one wins!"
-: playerOne === "paper" && computer === "rock" ? "Player one wins!"
-: playerOne === "scissors" && computer === "paper" ? "Player one wins!"
-: "computer wins!"; 
-console.log(result);
