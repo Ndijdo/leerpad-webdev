138fbd8e1dffd35fedeb92dc72db7aa3f822d757
N'djido <ardozuidberg@gmail.com>
2026-04-25 21:59:37 +0200

    Lesson 15 Completed JS

--- fase2/js/main.js
+++ fase2/js/main.js
@@ -1,56 +1,117 @@
-//  var, let and const
+// Arrays
+const myArray = []
 
-/* const x = 1;
-x = 2;
-console.log(x); */
+// add elements to an array
+myArray[0] = "ardo";
+myArray[1] = 1001;
+myArray[2] = false;
+console.log(myArray) 
 
-// global scope
-var x =1;
-//let y = 2;
-//const z = 3;
+// refer to an array
+console.log(myArray)
+
+// lenght property
+console.log(myArray.length)
+
+// last element in an array
+console.log(myArray[myArray.length - 1]);
+console.log(myArray[myArray.length - 2]);
+console.log(myArray[myArray.length - 3]);
+
+console.log(myArray[1]);
 
 
-// local scope
-{
-    let y = 4;
-    console.log(y);
-}
+// adding elements to an array
+myArray.push("school"); // adding elements to the end of the array
+myArray.unshift(42); // adding elements to the front of the array
 
-// local scope function 
-function myFunc () {
-    const z = 5;
-    console.log(z)
 
-    // local scope
-    {
-        let y = 6;
-        console.log(y);
-    }
-}
+console.log(myArray);
 
-myFunc();
 
-var x = 1; // Function scoped
-let y = 2; // Block scoped
-const z = 3; // Block scoped
+// remove elements from an array
+myArray.pop(); // removing elements from the back
+myArray.shift(); // removing elements from the beginning
 
-console.log(`global; ${x}`);
-console.log(`global; ${y}`);
-console.log(`global; ${z}`);
 
-function myFunc() {
-    var x = 10;
-    const z = 5;
-    {
-        var x = 11; // Funcion scoped
-        const z = 6; // block scoped
-        console.log(`block; ${x}`);
-        console.log(`block; ${y}`);
-        console.log(`block; ${z}`);
-    }
-    console.log(`function; ${x}`);
-    console.log(`function; ${y}`);
-    console.log(`function; ${z}`);
-}
+console.log(myArray);
 
-myFunc()
+
+// moving elements from an array to const
+const lastItem = myArray.pop();
+const firstItem = myArray.shift();
+console.log(firstItem)
+console.log(lastItem);
+console.log(myArray)
+
+
+// delelting elements from array
+delete myArray[0] // deleting element from array but elements stay in their spot.
+console.log(myArray)
+console.log(myArray[0])
+
+
+// removing and replacing elements from an array
+myArray.splice(1, 0, 42); // removing elements / replacing elements
+console.log(myArray)
+myArray.splice(2, 1, 42);
+console.log(myArray)
+
+
+// slicing arrays
+const myArray = ['A','B','C','D','E','F'];
+const newArray = myArray.slice(2,5);
+
+console.log(newArray)
+
+
+// reversing arrays
+const myArray = ['A','B','C','D','E','F'];
+myArray.reverse();
+
+console.log(myArray)
+
+
+// making an string from elements from an array
+const myArray = ['A','B','C','D','E','F'];
+
+const newString = myArray.join();
+
+const newArray = newString.split(",");
+
+console.log(newString) 
+console.log(newArray) 
+
+
+// adding different arrays together
+const myArrayA = ['A', 'B', 'C'];
+const myArrayB = ['D', 'E', 'F'];
+
+const newArray = myArrayA.concat(myArrayB);
+const newArrayA = [...myArrayA, ...myArrayB]
+
+console.log(newArrayA)
+
+
+// Nested arrays
+
+const equipShelfA = ["baseball", "football", "volleyball"];
+const equipShelfB = ["basketball", "golf balls", "tennis balls"];
+
+const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
+const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
+
+console.log(equipShelfA[1]);
+// or
+console.log(clothesShelfB[0]); 
+
+const equipDept = [equipShelfA, equipShelfB];
+const clothesDept = [clothesShelfA, clothesShelfB];
+
+console.log(equipDept[0][1]); // logging elements from a nested array
+console.log(clothesDept[1][0]);
+
+const sportStore = [equipDept, clothesDept]
+
+console.log(sportStore[0][0][1])
+console.log(sportStore[1][1][0])
