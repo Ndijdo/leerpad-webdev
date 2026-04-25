de1f816fa893b2b94e1be20b69331fc8750945de
N'djido <ardozuidberg@gmail.com>
2026-04-25 18:25:22 +0200

    Lesson 14 Completed JS

--- fase2/js/main.js
+++ fase2/js/main.js
@@ -1,35 +1,56 @@
-// Functions 
+//  var, let and const
 
-// Methods = Built-in Functions
-"Ardo".toLowerCase();
-Math.random();
+/* const x = 1;
+x = 2;
+console.log(x); */
+
+// global scope
+var x =1;
+//let y = 2;
+//const z = 3;
 
 
-// Function Declaration Syntax:
-function sum(num1, num2) {
-    if (num2 === undefined) {
-        return num1 + num1
-;    }
-    return num1 + num2;
+// local scope
+{
+    let y = 4;
+    console.log(y);
 }
-console.log(sum(5,10));
 
-function getUserNameFromEmail(email) { 
-    return email.slice(0, email.indexOf("@"));
+// local scope function 
+function myFunc () {
+    const z = 5;
+    console.log(z)
+
+    // local scope
+    {
+        let y = 6;
+        console.log(y);
+    }
 }
-console.log(getUserNameFromEmail("User@Github.com"));
 
-const getUserNameFromEmail = function (email) {
-    return email.slice(0, email.indexOf("@"));
+myFunc();
+
+var x = 1; // Function scoped
+let y = 2; // Block scoped
+const z = 3; // Block scoped
+
+console.log(`global; ${x}`);
+console.log(`global; ${y}`);
+console.log(`global; ${z}`);
+
+function myFunc() {
+    var x = 10;
+    const z = 5;
+    {
+        var x = 11; // Funcion scoped
+        const z = 6; // block scoped
+        console.log(`block; ${x}`);
+        console.log(`block; ${y}`);
+        console.log(`block; ${z}`);
+    }
+    console.log(`function; ${x}`);
+    console.log(`function; ${y}`);
+    console.log(`function; ${z}`);
 }
-console.log(getUserNameFromEmail("User1@Github.com"));
 
-const getUserNameFromEmail =  (email) => {
-    return email.slice(0, email.indexOf("@"));
-}
-console.log(getUserNameFromEmail("User2@Github.com"));
-
-const toProperCase = (name) => {
-    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-};
-console.log(toProperCase("ArDo"));
+myFunc()
